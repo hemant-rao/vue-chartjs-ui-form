a0bf392c9f5103edc4125b9dfe5c3e4b667a4578
mrhrao@gmail.com <mrhrao@gmail.com>
2024-10-08 00:55:43 +0530

    diffrent project run on same domain with diffrent folder name

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/chartjs/' // Yahaan 'vue-app' ko apne folder ke naam se replace karein
 })
